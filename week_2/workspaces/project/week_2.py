969978ec7bf4d038c0b5768354bdb16ffdc650c2
Deepika Singh <dsingh87@gmail.com>
2023-04-16 22:24:09 +0000

    week 2

--- week_2/workspaces/project/week_2.py
+++ week_2/workspaces/project/week_2.py
@@ -16,29 +16,38 @@ from workspaces.resources import mock_s3_resource, redis_resource, s3_resource
 from workspaces.types import Aggregation, Stock
 
 
-@op
-def get_s3_data():
-    pass
+@op(required_resource_keys={"s3"},
+    config_schema={"s3_key": String})
+def get_s3_data(context: OpExecutionContext) -> List[Stock]:
+    stocks = context.resources.s3.get_data(context.op_config["s3_key"])
+    return [Stock.from_list(i) for i in stocks]
 
 
 @op
-def process_data():
-    pass
+def process_data(context, stock_list: List[Stock]) -> Aggregation:
+    stock_dict = {}
+    for stock in stock_list:
+        stock_dict[stock.date] = stock.high
+    return(Aggregation(date = max(stock_dict, key=stock_dict.get), high = max(stock_dict.values())))
 
 
-@op
-def put_redis_data():
-    pass
+@op(required_resource_keys={"redis"},
+    ins={"aggregation": In(dagster_type=Aggregation)},)
+def put_redis_data(context, aggregation):
+    context.resources.redis.put_data(str(aggregation.date), str(aggregation.high))
 
 
-@op
-def put_s3_data():
-    pass
+@op(required_resource_keys={'s3'},
+    ins={"aggregation": In(dagster_type=Aggregation)})
+def put_s3_data(context, aggregation):
+    context.resources.s3.put_data(str(aggregation.date), aggregation.high)
 
 
 @graph
 def machine_learning_graph():
-    pass
+    aggregation = process_data(get_s3_data())
+    put_redis_data(aggregation)
+    put_s3_data(aggregation)    
 
 
 local = {
@@ -55,8 +64,12 @@ docker = {
 
 machine_learning_job_local = machine_learning_graph.to_job(
     name="machine_learning_job_local",
+    config = local,
+    resource_defs={"s3": mock_s3_resource, "redis": ResourceDefinition.mock_resource()},
 )
 
 machine_learning_job_docker = machine_learning_graph.to_job(
     name="machine_learning_job_docker",
+    config = docker,
+    resource_defs={"s3": s3_resource, "redis": redis_resource},
 )
